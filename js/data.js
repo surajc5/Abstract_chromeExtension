88fef2893d9a12fb93217a3ac527c0c4f7fe9f85
sunny <sunnychoudhury007@gmail.com>
2019-10-20 21:39:04 +0530

    add project data

---
+++ js/data.js
@@ -0,0 +1,401 @@
+var quotes = [
+    {
+        "quoteAuthor": "Thomas Edison",
+        "quoteText": "Genius is one percent inspiration and ninety-nine percent perspiration."
+    },
+    {
+        "quoteAuthor": "Yogi Berra",
+        "quoteText": "You can observe a lot just by watching."
+    },
+    {
+        "quoteAuthor": "Abraham Lincoln",
+        "quoteText": "A house divided against itself cannot stand."
+    },
+    {
+        "quoteAuthor": "Johann Wolfgang von Goethe",
+        "quoteText": "Difficulties increase the nearer we get to the goal."
+    },
+    {
+        "quoteAuthor": "Byron Pulsifer",
+        "quoteText": "Fate is in your hands and no one elses"
+    },
+    {
+        "quoteAuthor": "Lao Tzu",
+        "quoteText": "Be the chief but never the lord."
+    },
+    {
+        "quoteAuthor": "Carl Sandburg",
+        "quoteText": "Nothing happens unless first we dream."
+    },
+    {
+        "quoteAuthor": "Aristotle",
+        "quoteText": "Well begun is half done."
+    },
+    {
+        "quoteAuthor": "Yogi Berra",
+        "quoteText": "Life is a learning experience, only if you learn."
+    },
+    {
+        "quoteAuthor": "Margaret Sangster",
+        "quoteText": "Self-complacency is fatal to progress."
+    },
+    {
+        "quoteAuthor": "Buddha",
+        "quoteText": "Peace comes from within. Do not seek it without."
+    },
+    {
+        "quoteAuthor": "Byron Pulsifer",
+        "quoteText": "What you give is what you get."
+    },
+    {
+        "quoteAuthor": "Iris Murdoch",
+        "quoteText": "We can only learn to love by loving."
+    },
+    {
+        "quoteAuthor": "Karen Clark",
+        "quoteText": "Life is change. Growth is optional. Choose wisely."
+    },
+    {
+        "quoteAuthor": "Wayne Dyer",
+        "quoteText": "You'll see it when you believe it."
+    },
+    {
+        "quoteAuthor": "Unknown",
+        "quoteText": "Today is the tomorrow we worried about yesterday."
+    },
+    {
+        "quoteAuthor": "Unknown",
+        "quoteText": "It's easier to see the mistakes on someone else's paper."
+    },
+    {
+        "quoteAuthor": "Unknown",
+        "quoteText": "Every man dies. Not every man really lives."
+    },
+    {
+        "quoteAuthor": "Lao Tzu",
+        "quoteText": "To lead people walk behind them."
+    },
+    {
+        "quoteAuthor": "William Shakespeare",
+        "quoteText": "Having nothing, nothing can he lose."
+    },
+    {
+        "quoteAuthor": "Henry J. Kaiser",
+        "quoteText": "Trouble is only opportunity in work clothes."
+    },
+    {
+        "quoteAuthor": "Publilius Syrus",
+        "quoteText": "A rolling stone gathers no moss."
+    },
+    {
+        "quoteAuthor": "Napoleon Hill",
+        "quoteText": "Ideas are the beginning points of all fortunes."
+    },
+    {
+        "quoteAuthor": "Donald Trump",
+        "quoteText": "Everything in life is luck."
+    },
+    {
+        "quoteAuthor": "Lao Tzu",
+        "quoteText": "Doing nothing is better than being busy doing nothing."
+    },
+    {
+        "quoteAuthor": "Benjamin Spock",
+        "quoteText": "Trust yourself. You know more than you think you do."
+    },
+    {
+        "quoteAuthor": "Confucius",
+        "quoteText": "Study the past, if you would divine the future."
+    },
+    {
+        "quoteAuthor": "Unknown",
+        "quoteText": "The day is already blessed, find peace within it."
+    },
+    {
+        "quoteAuthor": "Sigmund Freud",
+        "quoteText": "From error to error one discovers the entire truth."
+    },
+    {
+        "quoteAuthor": "Benjamin Franklin",
+        "quoteText": "Well done is better than well said."
+    },
+    {
+        "quoteAuthor": "Ella Williams",
+        "quoteText": "Bite off more than you can chew, then chew it."
+    },
+    {
+        "quoteAuthor": "Buddha",
+        "quoteText": "Work out your own salvation. Do not depend on others."
+    },
+    {
+        "quoteAuthor": "Benjamin Franklin",
+        "quoteText": "One today is worth two tomorrows."
+    },
+    {
+        "quoteAuthor": "Christopher Reeve",
+        "quoteText": "Once you choose hope, anythings possible."
+    },
+    {
+        "quoteAuthor": "Albert Einstein",
+        "quoteText": "God always takes the simplest way."
+    },
+    {
+        "quoteAuthor": "Charles Kettering",
+        "quoteText": "One fails forward toward success."
+    },
+    {
+        "quoteAuthor": "Unknown",
+        "quoteText": "From small beginnings come great things."
+    },
+    {
+        "quoteAuthor": "Chinese proverb",
+        "quoteText": "Learning is a treasure that will follow its owner everywhere"
+    },
+    {
+        "quoteAuthor": "Socrates",
+        "quoteText": "Be as you wish to seem."
+    },
+    {
+        "quoteAuthor": "V. Naipaul",
+        "quoteText": "The world is always in movement."
+    },
+    {
+        "quoteAuthor": "John Wooden",
+        "quoteText": "Never mistake activity for achievement."
+    },
+    {
+        "quoteAuthor": "Haddon Robinson",
+        "quoteText": "What worries you masters you."
+    },
+    {
+        "quoteAuthor": "Pearl Buck",
+        "quoteText": "One faces the future with ones past."
+    },
+    {
+        "quoteAuthor": "Brian Tracy",
+        "quoteText": "Goals are the fuel in the furnace of achievement."
+    },
+    {
+        "quoteAuthor": "Leonardo da Vinci",
+        "quoteText": "Who sows virtue reaps honour."
+    },
+    {
+        "quoteAuthor": "Dalai Lama",
+        "quoteText": "Be kind whenever possible. It is always possible."
+    },
+    {
+        "quoteAuthor": "Chinese proverb",
+        "quoteText": "Talk doesn't cook rice."
+    },
+    {
+        "quoteAuthor": "Buddha",
+        "quoteText": "He is able who thinks he is able."
+    },
+    {
+        "quoteAuthor": "Larry Elder",
+        "quoteText": "A goal without a plan is just a wish."
+    },
+    {
+        "quoteAuthor": "Michael Korda",
+        "quoteText": "To succeed, we must first believe that we can."
+    },
+    {
+        "quoteAuthor": "Albert Einstein",
+        "quoteText": "Learn from yesterday, live for today, hope for tomorrow."
+    },
+    {
+        "quoteAuthor": "James Lowell",
+        "quoteText": "A weed is no more than a flower in disguise."
+    },
+    {
+        "quoteAuthor": "Yoda",
+        "quoteText": "Do, or do not. There is no try."
+    },
+    {
+        "quoteAuthor": "Harriet Beecher Stowe",
+        "quoteText": "All serious daring starts from within."
+    },
+    {
+        "quoteAuthor": "Byron Pulsifer",
+        "quoteText": "The best teacher is experience learned from failures."
+    },
+    {
+        "quoteAuthor": "Murray Gell-Mann",
+        "quoteText": "Think how hard physics would be if particles could think."
+    },
+    {
+        "quoteAuthor": "John Lennon",
+        "quoteText": "Love is the flower you've got to let grow."
+    },
+    {
+        "quoteAuthor": "Napoleon Hill",
+        "quoteText": "Don't wait. The time will never be just right."
+    },
+    {
+        "quoteAuthor": "Pericles",
+        "quoteText": "Time is the wisest counsellor of all."
+    },
+    {
+        "quoteAuthor": "Napoleon Hill",
+        "quoteText": "You give before you get."
+    },
+    {
+        "quoteAuthor": "Socrates",
+        "quoteText": "Wisdom begins in wonder."
+    },
+    {
+        "quoteAuthor": "Baltasar Gracian",
+        "quoteText": "Without courage, wisdom bears no fruit."
+    },
+    {
+        "quoteAuthor": "Aristotle",
+        "quoteText": "Change in all things is sweet."
+    },
+    {
+        "quoteAuthor": "Byron Pulsifer",
+        "quoteText": "What you fear is that which requires action to overcome."
+    },
+    {
+        "quoteAuthor": "Cullen Hightower",
+        "quoteText": "When performance exceeds ambition, the overlap is called success."
+    },
+    {
+        "quoteAuthor": "African proverb",
+        "quoteText": "When deeds speak, words are nothing."
+    },
+    {
+        "quoteAuthor": "Wayne Dyer",
+        "quoteText": "Real magic in relationships means an absence of judgement of others."
+    },
+    {
+        "quoteAuthor": "Albert Einstein",
+        "quoteText": "I never think of the future. It comes soon enough."
+    },
+    {
+        "quoteAuthor": "Ralph Emerson",
+        "quoteText": "Skill to do comes of doing."
+    },
+    {
+        "quoteAuthor": "Sophocles",
+        "quoteText": "Wisdom is the supreme part of happiness."
+    },
+    {
+        "quoteAuthor": "Maya Angelou",
+        "quoteText": "I believe that every person is born with talent."
+    },
+    {
+        "quoteAuthor": "Abraham Lincoln",
+        "quoteText": "Important principles may, and must, be inflexible."
+    },
+    {
+        "quoteAuthor": "Richard Evans",
+        "quoteText": "The undertaking of a new action brings new strength."
+    },
+    {
+        "quoteAuthor": "Ralph Emerson",
+        "quoteText": "The years teach much which the days never know."
+    },
+    {
+        "quoteAuthor": "Ralph Emerson",
+        "quoteText": "Our distrust is very expensive."
+    },
+    {
+        "quoteAuthor": "Bodhidharma",
+        "quoteText": "All know the way; few actually walk it."
+    },
+    {
+        "quoteAuthor": "Johann Wolfgang von Goethe",
+        "quoteText": "Great talent finds happiness in execution."
+    }
+];
+
+var morningImg = ['img/morning/m1.jpg','img/morning/m2.jpg','img/morning/m3.jpg','img/morning/m4.jpg',
+'img/morning/m5.jpg','img/morning/m6.jpg','img/morning/m7.jpg','img/morning/m8.jpg','img/morning/m9.jpg',
+'img/morning/m10.jpg','img/morning/m11.jpg','img/morning/m12.jpg','img/morning/m13.jpg','img/morning/m14.jpg',
+'img/morning/m15.jpg','img/morning/m16.jpg','img/morning/m17.jpg','img/morning/m18.jpg','img/morning/m19.jpg',
+'img/morning/m20.jpg','img/morning/m21.jpg','img/morning/m22.jpg','img/morning/m23.jpg','img/morning/m24.jpg',
+'img/morning/m25.jpg','img/s1.jpg','img/s2.jpg','img/s3.jpg','img/s4.jpg','img/s5.jpg','img/s6.jpg','img/s7.jpg','img/s8.jpg','img/s9.jpg'];
+
+var afternoonImg = ['img/afternoon/a1.jpg','img/afternoon/a2.jpg','img/afternoon/a3.jpg','img/afternoon/a4.jpg',
+'img/afternoon/a5.jpg','img/afternoon/a6.jpg','img/afternoon/a7.jpg','img/afternoon/a8.jpg','img/afternoon/a9.jpg',
+'img/afternoon/a10.jpg','img/afternoon/a11.jpg','img/afternoon/a12.jpg','img/afternoon/a13.jpg',
+'img/afternoon/a14.jpg','img/afternoon/a15.jpg','img/afternoon/a16.jpg','img/afternoon/a17.jpg',
+'img/afternoon/a18.jpg','img/afternoon/a19.jpg','img/afternoon/a20.jpg','img/afternoon/a21.jpg',
+'img/afternoon/a22.jpg','img/afternoon/a23.jpg','img/afternoon/a24.jpg','img/afternoon/a25.jpg',
+'img/afternoon/a26.jpg','img/afternoon/a27.jpg','img/afternoon/a28.jpg','img/afternoon/a29.jpg',
+'img/afternoon/a30.jpg','img/afternoon/a31.jpg','img/afternoon/a32.jpg','img/afternoon/a33.jpg',
+'img/afternoon/a34.jpg','img/afternoon/a35.jpg','img/afternoon/a36.jpg','img/afternoon/a37.jpg',
+'img/s1.jpg','img/s2.jpg','img/s3.jpg','img/s4.jpg','img/s5.jpg','img/s7.jpg','img/s8.jpg']  ;
+
+var eveningImg = ['img/evening/e1.jpg','img/evening/e2.jpg','img/evening/e3.jpg','img/evening/e4.jpg',
+'img/evening/e5.jpg','img/evening/e6.jpg','img/evening/e7.jpg','img/evening/e8.jpg','img/evening/e9.jpg',
+'img/evening/e10.jpg','img/evening/e11.jpg','img/evening/e12.jpg','img/evening/e13.jpg',
+'img/evening/e14.jpg','img/evening/e15.jpg','img/evening/e16.jpg','img/evening/e17.jpg',
+'img/evening/e18.jpg','img/evening/e19.jpg','img/evening/e20.jpg','img/evening/e21.jpg',
+'img/evening/e22.jpg','img/evening/e23.jpg','img/evening/e24.jpg','img/evening/e25.jpg',
+'img/evening/e26.jpg','img/evening/e27.jpg','img/evening/e28.jpg','img/evening/e29.jpg',
+'img/evening/e30.jpg','img/evening/e31.jpg',
+'img/s1.jpg','img/s2.jpg','img/s3.jpg','img/s4.jpg','img/s5.jpg','img/s6.jpg','img/s9.jpg'];
+
+var nightImg = ['img/night/n1.jpg','img/night/n2.jpg','img/night/n3.jpg','img/night/n4.jpg','img/night/n5.jpg','img/night/n6.jpg',
+'img/night/n7.jpg','img/night/n8.jpg','img/night/n9.jpg'];              
+
+
+// ...................... light theme for todo ...................
+
+function todoLight () {
+let todo = document.querySelector('.todo');
+todo.style.color = 'rgba(252, 252, 252, 0.8)';
+let time = document.querySelector('#time');
+let greetings = document.getElementById('greetings');
+let userName = document.querySelector('.userName');
+let en = document.querySelector('.en');
+en.style.color = "#fff";
+userName.style.color = '#fff';
+userName.style.webkitTextStroke = "transparent";
+time.style.color = '#fff';
+time.style.webkitTextStroke = "transparent";
+greetings.style.color ="#fff";
+greetings.style.webkitTextStroke = "transparent";
+let quote = document.querySelector('.quote');
+quote.style.color = "#fff";
+let quoteText = document.querySelector('.authorName');
+quoteText.style.color = "#fff";
+document.querySelector('.footer1').classList.add('footer2');
+let input = document.querySelector('.input_container');
+input.firstElementChild.style.color = 'rgb(0,0,0)';
+input.firstElementChild.className = 'inputBox';
+input.firstElementChild.style.boxShadow = 'none';
+input.firstElementChild.style.backgroundColor = 'rgb(255,255,255,0.6)';
+}
+
+function todoLightMorning() {
+
+let todo = document.querySelector('.todo');
+todo.style.color = 'rgba(252, 252, 252, 0.8)';
+let time = document.querySelector('#time');
+let greetings = document.getElementById('greetings');
+let userName = document.querySelector('.userName');
+let en = document.querySelector('.en');
+en.style.color = "#fff";
+userName.style.color = '#fff';
+userName.style.webkitTextStroke = "transparent";
+time.style.color = '#fff';
+time.style.webkitTextStroke = "transparent";
+greetings.style.color ="#fff";
+greetings.style.webkitTextStroke = "transparent";
+let quote = document.querySelector('.quote');
+quote.style.color = "#fff";
+let quoteText = document.querySelector('.authorName');
+quoteText.style.color = "#fff";
+}
+
+function todoDark (){
+let liList = document.querySelectorAll('.li_list');
+liList.forEach(e => {
+    e.style.backgroundColor = 'rgba(0,0,0,0.6)';
+    e.style.color = '#fff';
+    e.childNodes[0].childNodes[0].style.backgroundColor = 'transparent';
+    e.childNodes[0].childNodes[0].childNodes[0].src = 'chk.png';
+})
+}
---
+++ js/main.js
@@ -0,0 +1,408 @@
+// You js goes here
+
+// .........................welcome ..........................
+
+let hey = document.querySelector('.hey');
+let passwordInput = document.querySelector('.createPasswordInput');
+let verifyInput = document.querySelector('.verifyPasswordInput');
+let enterName = document.querySelector('.enterName');
+let userName = document.querySelector('.userName');
+let containerAll = document.querySelector('.containerAll');
+let login1 = document.querySelector('.login');
+let createPasswordContainer = document.querySelector('.passwordCreate');
+let verifyPasswordContainer = document.querySelector('.passwordVerify');
+let inputArr = JSON.parse(localStorage.getItem('arr-list')) || [];
+console.log(inputArr);
+
+let inputValue = {
+    name: "",
+    password: ""
+};
+
+function checkLogin (){
+
+    if (inputArr.length && inputArr[0].password != ""){
+        login1.classList.add('login1');
+        createPasswordContainer.classList.add('passwordCreateRemove');
+        verifyPasswordContainer.classList.add('passwordVerifyRemove');
+        console.log('input Arr inside chek',inputArr)
+        name = inputArr[0].name
+        userName.innerText = name[0].toUpperCase() + name.slice(1);
+        console.log('username',name[0].toUpperCase() + name.slice(1))
+        document.body.classList.add('body');
+        containerAll.classList.add('allContainer');
+    }
+    else{
+        console.log('before set state')
+        inputArr = []
+        localStorage.setItem('arr-list',JSON.stringify(inputArr))
+    }
+}
+checkLogin();
+
+function login (event) {
+    if (event.keyCode === 13 && event.target.value.trim() != ""){
+        inputValue['name'] = event.target.value;
+        name = inputValue['name']
+        userName.innerText = name[0].toUpperCase() + name.slice(1);
+        inputArr.push(inputValue);
+        login1.classList.add('login1');
+        createPasswordContainer.classList.add('passwordCreateAdd');
+        console.log('im in');
+        hey.value = "";
+        // inputView(inputArr) 
+    }
+    localStorage.setItem('arr-list', JSON.stringify(inputArr));
+}
+
+function createPassword (event) {
+
+    if (event.keyCode === 13 && event.target.value.trim() != ""){
+        inputValue['password'] = event.target.value;
+        inputArr.push(inputValue);
+        createPasswordContainer.classList.add('passwordCreateRemove');
+        verifyPasswordContainer.classList.add('passwordVerifyAdd');
+        passwordInput.value = "";
+    }
+    localStorage.setItem('arr-list', JSON.stringify(inputArr));
+}
+console.log(inputArr);
+
+// TEST
+console.log(localStorage, 'Check local storage')
+// if (localStorage)
+// 
+function verifyPassword (event) {
+    if (event.keyCode === 13 && event.target.value.trim() != ''){
+        let match = event.target.value;
+        console.log('lol', match, inputArr[0].password);
+        if (match == inputArr[0].password) {
+        verifyPasswordContainer.classList.add('passwordVerifyRemove');
+        document.body.classList.add('body');
+        containerAll.classList.add('allContainer');
+        } 
+    }
+    localStorage.setItem('arr-list', JSON.stringify(inputArr));   
+    console.log('outside lol');
+}
+
+    
+// ..........................Clock...............................
+setInterval(displayTime,500);
+function displayTime() {
+    let time = new Date();
+    let hours = time.getHours();
+    let mints = time.getMinutes();
+    let greetings = document.getElementById('greetings');
+
+    let en = document.querySelector('.en');
+    if (hours > 12 ){
+
+       EN = en .innerText = 'PM';
+
+    } else if (hours < 12 ) {
+
+       EN =  en.innerText =  'AM';
+    }
+
+    if (hours >= 12 && hours < 17) {
+        if(greetings.innerText !== "Good afternoon,") {
+            todoDark()
+            afternoonBg () 
+        }
+        greetings.innerText = "Good afternoon,";
+    } 
+
+    if (hours >= 17 && hours <= 21) {
+
+        if(greetings.innerText !== "Good evening,") {
+            todoLight()
+            eveningBg ()  
+        }
+        greetings.innerText = "Good evening,";
+       
+    }
+    
+    if (hours > 21) {
+        if(greetings.innerText !== "Good night,") {
+            todoLight()
+            nightBg () 
+        }
+        greetings.innerText = "Good night,";
+    } 
+
+     if (hours < 12) {
+        if(greetings.innerText !== "Good morning,") {
+            todoLightMorning()
+            morningBg () 
+        }
+        greetings.innerText = "Good morning,";
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    if (hours == 0) {
+        hours = 12;
+    } 
+    // if (hours < 10) {
+    //     hours = '0' + hours;
+    // }
+    if (mints < 10) {
+        mints = '0' + mints;
+    // }
+    // if (seconds < 10) {
+    //     seconds = '0' + seconds;
+    // }
+    }
+    document.getElementById('time').innerHTML = hours + ':' + mints ;
+    
+}
+//.......................................
+
+
+
+
+//............................random background......................... 
+
+function morningBg() {
+
+let selectBG = morningImg[Math.floor(Math.random() * morningImg.length)];
+document.querySelector('.body').style.backgroundImage = 'url(' + selectBG + ')';
+
+}
+function afternoonBg() {
+
+    let selectBG = afternoonImg[Math.floor(Math.random() * afternoonImg.length)];
+    document.querySelector('.body').style.backgroundImage = 'url(' + selectBG + ')';
+    
+}
+function eveningBg() {
+
+    let selectBG = eveningImg[Math.floor(Math.random() * eveningImg.length)];
+    document.querySelector('.body').style.backgroundImage = 'url(' + selectBG + ')';
+    
+}
+function nightBg() {
+
+    let selectBG = nightImg[Math.floor(Math.random() * nightImg.length)];
+    document.querySelector('.body').style.backgroundImage = 'url(' + selectBG + ')';
+    
+}
+
+
+//........................................
+
+
+
+
+// .................................quotes............................
+
+
+
+
+
+let quote = document.querySelector('.quote'); 
+let text = document.querySelector('.authorName');
+
+function randomQuotes () {
+    let randomNum = Math.round(Math.random() * quotes.length);
+    quote.innerHTML = quotes[randomNum].quoteText;
+    text.innerHTML = quotes[randomNum].quoteAuthor;
+}
+setInterval(randomQuotes, 8000);
+randomQuotes();
+
+
+
+
+
+// ...............................todo.................................
+
+
+let input = document.querySelector("#input");
+let inputContainer = document.querySelector(".input_container");
+let ul = document.querySelector(".list");
+let activeTodo = document.querySelector(".active");
+let completedTodo = document.querySelector(".completed");
+let clearCompletedTodo = document.querySelector(".clear_completed");
+let item_count = document.querySelector('.item_count')
+let item = document.querySelector('.item');
+let allTodo = document.querySelector('.all');
+let allTodo2 = document.querySelector('.all1');
+let selectAllTodo = document.querySelector('.i');
+var newlabel = document.createElement("Label");
+let footer1 = document.querySelector('footer1');
+// todoList =[];
+let todoList = JSON.parse(localStorage.getItem('todo-list')) || [];
+
+function view (arrayToDisplay) {
+    ul.innerHTML = '';
+    
+    arrayToDisplay.forEach((item,index) => {
+        let li = document.createElement("li");
+        li.className = "li_list";
+        const check = document.createElement("input");
+        check.type = "checkbox";
+        check.setAttribute("data-id", todoList.indexOf(item));
+        check.id = 'right-'+index;
+        check.className = "check";
+        const newLabel = document.createElement("Label");
+        newLabel.setAttribute("for",'right-'+index);
+        imgContainer = document.createElement("div");
+        imgContainer.className = 'imgContainer';
+        img = document.createElement('img');
+        img.className = 'right';
+        img.src = 'chk1.png';
+        imgContainer.appendChild(img);
+        newLabel.appendChild(imgContainer);
+        li.appendChild(newLabel);
+        check.checked = item.isDone;
+        check.addEventListener("click", handleCheck);
+        const p = document.createElement("p");
+        p.innerText = item.text;
+        const btn = document.createElement("span");
+        btn.addEventListener("click" , deleteSubmit);
+        btn.innerText = "x";
+        btn.className = "btn del";
+        btn.setAttribute("data-id", index);
+        if (item.isDone == true ){
+            img.style.opacity = "1";
+            imgContainer.style.border = "none";
+            p.style.textDecoration = "line-through";
+            p.style.color = "rgba(0, 0, 0,0.4)";
+        } else {
+            img.style.opacity = "0";
+            p.style.textDecoration = "none";
+        }
+        li.appendChild(check);
+        li.appendChild(p);
+        li.appendChild(btn);
+        ul.appendChild(li);
+    });
+    
+    item_count.innerText = activeLength();
+    itemsLeft(); 
+    showClearCompleted (); 
+    localStorage.setItem('todo-list', JSON.stringify(todoList));
+}
+
+function handleSubmit (event){
+    if (event.keyCode === 13 && event.target.value.trim() != "") {
+        const todoText = {text: '', isDone: false , id:''};
+        todoText.text = input.value;
+        todoList.push(todoText);
+        input.style.border = "none";
+        console.log(todoList);
+        document.querySelector(".input_container .i").classList.add("i_");
+        document.querySelector(".input_container .i").style.opacity = 0.2;
+        document.querySelector(".foot").classList.add("footer_container");
+        event.target.value = "";
+        view(todoList);
+    }
+}    
+    function deleteSubmit (event) {
+        console.log('inside delete',event)
+        var index = event.target.dataset.id
+        // if(event.target.classList.contains("del")){
+            console.log('inside delete if')
+        todoList.splice(index,1);
+        // }
+        view(todoList);
+
+    }
+    
+
+    function handleCheck (event) {
+        console.log("handle check",event);
+        var id = event.target.dataset.id;
+        console.log(id);
+        todoList[id].isDone = !todoList[id].isDone;
+        view(todoList)
+
+}
+    function completed () {
+        console.log('inside complete')
+        const isChecked = todoList.filter((item) => {
+            allTodo2.classList.remove('all');
+            activeTodo.classList.remove('active1');
+            completedTodo.classList.add('completed1');
+            return item.isDone == true;
+        })
+        view(isChecked);
+}
+    function showClearCompleted () {
+        if(todoList.some((todo) => todo.isDone==true)) {
+            document.querySelector('.clear_completed').classList.add('clearCompleted');
+        }else{
+            document.querySelector('.clear_completed').classList.remove('clearCompleted');
+        }
+};
+ 
+   function active () {
+        const isActive = todoList.filter((item) => {
+            completedTodo.classList.remove('completed1');
+            allTodo2.classList.remove('all');
+            activeTodo.classList.add('active1');
+            return item.isDone == false;
+        })
+        view(isActive);
+    }
+    function activeLength () {
+        const isActive = todoList.filter((item) => {
+            return item.isDone == false;
+        })
+        
+        return isActive.length;
+    }
+    function all() {
+        completedTodo.classList.remove('completed1');
+        allTodo2.classList.add('all');
+        activeTodo.classList.remove('active1');
+        view(todoList);
+    }
+    function itemsLeft () {
+        const isActive = todoList.filter((item) => {
+            return item.isDone == false;
+        })
+        return isActive.length <= 1 ? item.innerText = "item left" : item.innerText = "items left";
+        }
+    function clearCompleted (event) {
+        const isNotComplete = todoList.filter((item) => {
+            selectAllTodo.style.opacity = 0.2;
+             return item.isDone == false;
+          })
+          todoList = isNotComplete;
+          view(todoList)
+    }
+    function selectAll (event){
+        const falsed = todoList.filter(item => item.isDone == false)
+        console.log(falsed.length); 
+        if(falsed.length == 0){
+            todoList.forEach((item) => {
+                selectAllTodo.style.opacity = 0.2;
+                return item.isDone = false;
+            })            
+        }
+        else {
+            todoList.forEach((item) => {
+                selectAllTodo.style.opacity = 0.8;
+                return item.isDone = true;
+            })
+        }
+        
+        view(todoList);
+    }
+
+    view(todoList);
+    enterName.addEventListener('keyup',login);
+    passwordInput.addEventListener('keydown',createPassword);
+    verifyInput.addEventListener('keydown',verifyPassword);
+    input.addEventListener('keydown', handleSubmit);
+    completedTodo.addEventListener('click',completed);
+    activeTodo.addEventListener('click',active);
+    allTodo.addEventListener("click",all);
+    clearCompletedTodo.addEventListener('click',clearCompleted);
+    selectAllTodo.addEventListener('click',selectAll);
+
+
+    // ...............................todo end ...........................
